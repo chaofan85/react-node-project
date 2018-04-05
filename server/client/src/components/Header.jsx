2a3169469082616936d8f278ce360e89ceb49fbf
Chao Fan <fanchao05@gmail.com>
2018-04-04 22:42:04 -0400

    updated Header component

--- server/client/src/components/Header.jsx
+++ server/client/src/components/Header.jsx
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a>Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     return (
       <nav>
@@ -8,10 +28,8 @@ class Header extends Component {
           <a href="#" className="left brand-logo">
             Emaily
           </a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li>
-              <a href="#">Login With Google</a>
-            </li>
+          <ul id="nav-mobile" className="right">
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -19,4 +37,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
